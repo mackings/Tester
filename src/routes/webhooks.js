38ee2d2142fe950160aad77e492966de3eb68a1d
mackingsley <kingsleyudoma2018@gmail.com>
2024-07-05 02:36:51 +0100

    socket

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -232,7 +232,7 @@ router.post('/paxful/webhook', async (req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Socket Running on 3000');
 });
 
 module.exports = router;
